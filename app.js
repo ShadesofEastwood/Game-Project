d66302d1bbb8e925f7df81bbb9b9a38742571e90
ShadesofEastwood <aldentonelli@gmail.com>
2022-02-14 16:25:18 -0500

    created stardust crusaders deck

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 console.log("ZA WARUDO");
 
-/* These DOM manipulation for opening the modal. */
+/* The flashCard class will be used to create keys that correspond to the flash cards keyword and its defintion. Using this.key we can populate the modal easily.*/
+
+class flashCard {
+    constructor(keyword, definition) {
+      this.keyword = keyword;
+      this.definition = definition;
+      this.value = "incorrect";
+    }
+  }
+  
+  /* These are where the flashcards are created and stored into an array for easy access. */
+  
+    theHermit = new flashCard("The Hermit", "Hermit Purple is the Stand of Joseph Joestar. It is a Close-Range Stand.")
+
+    theStar = new flashCard("The Star", "Star Platinum is the Stand of Jotaro Kujo. It is a Close-Range Stand.")
+
+    theChariot = new flashCard("The Chariot", "Silver Chariot is the Stand of Jean Pierre Polnareff. It is a Close-Range Stand.")
+
+    theHierophant = new flashCard("The Hierophant", "Hierophant Green is the Stand of Noriaki Kakyoin. It is a Long-Distance Stand.")
+
+    theMagician = new flashCard("The Magician", "Magician's Red is the Stand of Muhammad Avdol. It is a Close-Range Stand.")
+
+    theFool = new flashCard("The Fool", "The Fool is the Stand of Iggy. It is a Close-Range Stand.")
+  
+  
+  deck1 = [theHermit,theStar,theChariot,theHierophant,theFool,theMagician]
+  deck2 = []
+  deck3 = []
+
+/* These DOM manipulation are for opening the modal and loading the modal with content fom our arrays. */
 
 document.getElementById("startButton").addEventListener("click", function () {
   document.getElementById("modal").style.display = "block";
@@ -9,60 +38,11 @@ document.getElementById("startButton").addEventListener("click", function () {
 
 function startGame() {
   document.getElementById("modal-content").innerText = `${
-    contentArray[contentArray.length - 1].keyword
+    deck1[deck1.length - 1].keyword
   }`;
 }
 
-/* The flashCard class will be used to create keys that correspond to the flash cards keyword and its defintion. Using this.key we can populate the modal easily.*/
 
-class flashCard {
-  constructor(keyword, definition) {
-    this.keyword = keyword;
-    this.definition = definition;
-    this.value = "incorrect";
-  }
-}
-
-/* These are where the flashcards are created and stored into an array for easy access. */
-
-
-
-deck1 = []
-deck2 = []
-deck3 = []
-
-/*
-spiderMan = new flashCard("Spider-man", "Peter Parker");
-
-ironMan = new flashCard("Iron Man", "Tony Stark");
-
-thor = new flashCard("Thor", "Dr. Donald Blake")
-
-hawkEye = new flashCard("Hawkeye", "Clint Barton")
-
-blackWidow = new flashCard("Black Widow", "Natasha Roanov")
-
-hulk = new flashCard("Hulk", "Bruce Banner")
-
-captainAmerica = new flashCard("Captain America", "Steve Rogers")
-
-*/
-
-contentArray = [spiderMan, ironMan, thor, hawkEye, blackWidow, hulk, captainAmerica];
-
-/* This section is the DOM manipulations that fill the modal with information from the array above*/
-
-/* function populate () {
-    console.log(this)
-    if (this.value == "incorrect") {
-        document.getElementById("modal-content").innerText = `${this.keyword}`
-    }
-    
-}
-
-
-contentArray[0].populate()
-contentArray[1].populate() */
 
 /* This section below is how the user can operate the flash cards. Either switching to the next one or marking the flash card correct or incorrect.*/
 
@@ -70,21 +50,45 @@ document.onkeydown = function (event) {
   switch (event.keyCode) {
     case 37:
       console.log("Left key is pressed.");
-      document.getElementById("modal-content").innerText = contentArray[contentArray.length -1].keyword
+      document.getElementById("modal-content").innerText = deck1[deck1.length -1].keyword
       break;
     case 38:
       console.log("Up key is pressed.");
-      contentArray[contentArray.length -1].value = "correct"
+      deck1[deck1.length -1].value = "correct"
       break;
     case 39:
       console.log("Right key is pressed.")
-      document.getElementById("modal-content").innerText = contentArray[contentArray.length -1].definition;
+      document.getElementById("modal-content").innerText = deck1[deck1.length -1].definition;
       break;
     case 40:
       console.log("Down key is pressed.");
-      contentArray.pop()
-      document.getElementById("modal-content").innerText = contentArray[contentArray.length -1].keyword
+      deck1.pop()
+      document.getElementById("modal-content").innerText = deck1[deck1.length -1].keyword
       break;
       
   }
 };
+
+/*The myTimer function is for keeping track of how long the user takes to complete the flashcards. It adds 1 to a number variable and then using DOM manipulation, places that new number into the document every second.*/
+
+var timer = setInterval(myTimer,1000)
+
+var seconds = 0
+var minutes = 0
+
+document.getElementById("timer").innerText = `Timer: ${minutes} minutes and ${seconds} seconds`
+
+function myTimer(){
+   seconds = seconds + 1
+
+   if (seconds >= 60) {
+       minutes = minutes + 1
+       seconds = 0
+   }
+   
+   document.getElementById("timer").innerText = `Timer: ${minutes} minutes and ${seconds} seconds`
+
+
+}
+
+myTimer()
